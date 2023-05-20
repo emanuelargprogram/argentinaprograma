4f4a875b90479812729d47c036fb4c19926fef1d
emanuelargprogram <emanuel68@live.com.ar>
2023-05-20 12:10:54 -0300

    borre por falta de commit

--- practico0/areaRectangulo.js
+++
@@ -1,8 +0,0 @@
-let base= ;
-let altura= ;
-let area= base * altura;
-console.log(area);
-
-/* 2.a  el resultado es 15 */
-
-/* 2.b el resultado es 66.25 */
--- practico0/conversionGrados.js
+++
@@ -1,6 +0,0 @@
-let F= ;
-let C= (F - 32)*(5/9);
-console.log(C);
-
-/* 3.a El resultado es 10*/
-/* 3.b el resultado es 23.88888888888889 */
