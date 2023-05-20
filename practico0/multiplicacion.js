a98c6862d67776b730571481ca68c2718e2ee0dc
emanuelargprogram <emanuel68@live.com.ar>
2023-05-20 11:36:27 -0300

    segundo commit

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,7 @@
+let num1= ;
+let num2= ;
+let num3= ;
+let multiplicacion= num1 * num2 * num3;
+console.log(multiplicacion);
+
+/*1 .b El resultado es 204 */
---
+++ practico0/resta.js
@@ -0,0 +1,6 @@
+let num1= ;
+let num2=;
+let resta=num1 - num2 ;
+console.log(resta);
+
+/*1 .a El resultado es 8*/
