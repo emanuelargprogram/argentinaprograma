d259035d17da0de62cd50394e887e0679c26f0ae
emanuelargprogram <emanuel68@live.com.ar>
2023-05-20 11:43:14 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,8 @@
+let base= ;
+let altura= ;
+let area= base * altura;
+console.log(area);
+
+/* 2.a  el resultado es 15 */
+
+/* 2.b el resultado es 66.25 */
---
+++ promedio3.js
@@ -0,0 +1,10 @@
+let num1= ;
+let num2= ;
+let num3= ;
+let promedio=(num1 + num2 + num3)/3 ;
+console.log(promedio);
+
+
+/* 4 .a El resultado es 7 */
+/* 4 .b El resultado es -4.333333333333333 */
+/* 4 .c El resultado es 0.2666666666666666*/
